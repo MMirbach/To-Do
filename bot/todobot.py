498608822d7291795d655379e8b7542e18bfebe3
MMirbach <70845604+MMirbach@users.noreply.github.com>
2021-12-24 22:00:27 +0200

    typo

--- bot/todobot.py
+++ bot/todobot.py
@@ -39,7 +39,7 @@ def get_username(update, context):
         return EXPECT_PASSWORD
     else:
         update.message.reply_text(f"Sorry, I don't know anyone called {username}, please tell me your active username or "
-                                  "go to http://132.69.8.12 to create a one and come back to tell me")
+                                  "go to http://132.69.8.12 to create one and come back to tell me")
         return EXPECT_USERNAME
 
 
